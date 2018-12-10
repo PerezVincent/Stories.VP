2d441c50055e1fae15c43a4ac85b2e928ffffa8a
Vincent <perez_vincent@ymail.com>
2018-12-10 16:12:20 +0100

    Appel login et password

--- src/user/login.class.js
+++ src/user/login.class.js
@@ -6,8 +6,12 @@ class Login {
        // Modifier le titre de la page
        $('#main-title').html('Identifiez-vous');
 
+        // Définition des attributs
+        this.login = $('[name="loginField"]');
+        this.password = $('[name="passwordField"]');
        // Définition de listener sur le formulaire
        this.formListener();
+       this.submitListener();
     }
 
     /**formListener Gestionnaire d'évènement sur le formulaire de login
@@ -16,13 +20,17 @@ class Login {
      */
 
     formListener() {
+
+        let login = this.login;
+        let password = this.password;
+        
         $('#loginForm').on(
             'keyup',
             // Callback : fonction appelée si l'évènement défini survient
             function(event) {
                 // Vérifier le contenu des deux champs
-                const login = $('[name="loginField"]');
-                const password = $('[name="passwordField"]');
+                //const login = $('[name="loginField"]');
+                //const password = $('[name="passwordField"]');
 
                 // Est-ce que les 2 champs sont remplis
                 if(
@@ -36,5 +44,47 @@ class Login {
             }
         );
     }
+
+    submitListener() {
+
+        let login = this.login;
+        let password = this.password;
+        
+        $('#loginForm').on(
+            'submit',
+            function(event) {
+                event.preventDefault(); // Empêche l'action par défaut...
+
+                // const login = $('[name=loginField]').val();
+                // const password = $('[name="passwordField"]').val();
+
+                // Instancie un nouvel utilisateur
+                const user = new User();
+
+                // Définit le login et le password de l'utilisateur
+                user.setUserName(this.login.val());
+                user.setPassword(this.password.val());
+
+                
+                //  AUTRE METHODE : on peut utiliser directement les $() dans le user.set
+                // user.setUserName($('[name=loginField]').val());
+                // user.setPassword($('[name="passwordField"]').val());
+                // DANS CE CAS, on n'est pas obligé de définir préalablement les constantes : $('[...])
+                
+                // Gère l'authentification...
+                if (user.authenticate()) {
+                    console.log('Oki, tu peux y aller');
+                } else {
+                    console.log('ko, t\'as pas le droit');
+                    
+                    $('[name="loginField"]').val('');
+                    $('[name="passwordField"]').val('');
+                    $('#btnLogin').attr('disabled', 'disabled');
+
+                }
+            }
+        );
+    }
+
 
 }
